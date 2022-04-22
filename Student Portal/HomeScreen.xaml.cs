d8f3d92099345d9cd41243d59e3a20c580545a1d
Lindo Gamede <lindohgamede@gmail.com>
2022-04-22 16:01:24 +0200

    Added more views and viewmodels.

--- Student Portal/HomeScreen.xaml.cs
+++ Student Portal/HomeScreen.xaml.cs
@@ -25,20 +25,36 @@ namespace Student_Portal
             InitializeComponent();
         }
 
+        private void HomeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new HomeViewModel();
+            SelectedItemLbl.Content = "Home";
+        }
+
+        private void CreateUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new CreateUserAccountViewModel();
+            SelectedItemLbl.Content = "Create New User Account";
+        }
+
+        private void UpdateUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new UpdateUserAccountViewModel();
+            SelectedItemLbl.Content = "Update User Account";
+        }
+
+        private void DeleteUserBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DataContext = new DeleteUserAccountViewModel();
+            SelectedItemLbl.Content = "Delete User Account";
+        }
+
         private void UpdateAdminBtn_Click(object sender, RoutedEventArgs e)
         {
             DataContext = new AdminAccontViewModel();
             SelectedItemLbl.Content = "Admin Personal Details";
         }
 
-        private void CreateUserBtn_Click(object sender, RoutedEventArgs e)
-        {
-            DataContext = new CreateUserAccountViewModel();
-        }
-
-        private void ContentControl_Scroll(object sender, System.Windows.Controls.Primitives.ScrollEventArgs e)
-        {
-
-        }
+       
     }
 }
